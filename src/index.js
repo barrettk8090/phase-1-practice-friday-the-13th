cdc29b31c5e5565b3d2007cd43d4d2f2798bb09f
Barrett Kowalsky <barrettkowalsky@gmail.com>
2023-11-04 17:04:29 -0600

    complete challenge 3 - clicking on nav updates the movie that appears in main

--- src/index.js
+++ src/index.js
@@ -12,7 +12,12 @@ function createMovieImgForNav(movie){
 
     nav.appendChild(img);
     img.src = movie["image"];
-}
+    img.id = "test"
+
+    img.addEventListener("click", function () {
+        addMovieToMain(movie);
+    })
+} 
 
 
 fetch("http://localhost:3000/movies/1",{
@@ -33,10 +38,27 @@ function addMovieToMain(movie){
     const releaseYear = document.querySelector("#year-released");
     const description = document.querySelector("#description");
     const image = document.querySelector("#detail-image");
+    const watched = document.querySelector("#watched");
+    const bloodAmount = document.querySelector("#amount");
 
     title.textContent = movie.title;
     releaseYear.textContent = movie.release_year;
     description.textContent = movie.description;
     image.src = movie.image;
+    bloodAmount.textContent = movie.blood_amount;
+
+    if (movie.watched === false){
+        watched.innerText = "Unwatched";
+    }
 }
+
+
+// fetch("http://localhost:3000/movies")
+// .then((r) => r.json())
+// .then(data => {
+//     const movies = data;
+//     movies.forEach((movie => createMovieImgForNav(movie)));
+// });
+
+// document.querySelector("img").addEventListener("click", function(){ console.log("I was clicked")})
 
